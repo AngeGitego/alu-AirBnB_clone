d1546f010c45c54084341e8e700574ade6650770
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2024-05-19 03:40:37 -0700

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-"""This module creates a User class"""
-
+"""
+Module for the State class.
+"""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """Class for managing state objects"""
+    """
+    Represent a state.
 
-    name = "
+    Attributes:
+        name (str): The name of the state.
+    """
+
+    name = ""
