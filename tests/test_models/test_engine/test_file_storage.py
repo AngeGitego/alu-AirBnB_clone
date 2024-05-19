a09d841cc8d9b41249a50395321311120730aede
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2024-05-19 03:28:20 -0700

    next commit

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,171 +1,77 @@
 #!/usr/bin/python3
 """
-Module for FilStorage unittest
+Module for serializing and deserializing data
 """
-import os
 import json
-import models
-import unittest
+import os
 from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
 from models.user import User
-from models.state import State
-from models.place import Place
-from models.city import City
 from models.amenity import Amenity
+from models.place import Place
 from models.review import Review
+from models.state import State
+from models.city import City
 
 
-class TestFileStorage_instantiation(unittest.TestCase):
+class FileStorage:
     """
-    Unittests for testing instantiation of the FileStorage class.
+    FileStorage class for storing, serializing and deserializing data
     """
+    __file_path = "file.json"
 
-    def test_FileStorage_instantiation_no_args(self):
-        self.assertEqual(type(FileStorage()), FileStorage)
+    __objects = {}
 
-    def test_FileStorage_instantiation_with_arg(self):
-        with self.assertRaises(TypeError):
-            FileStorage(None)
+    def new(self, obj):
+        """
+         Sets an object in the __objects dictionary with a key of 
+         <obj class name>.id.
+        """
+        obj_cls_name = obj.__class__.__name__
 
-    def test_FileStorage_file_path_is_private_str(self):
-        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
+        key = "{}.{}".format(obj_cls_name, obj.id)
 
-    def testFileStorage_objects_is_private_dict(self):
-        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
-
-    def test_storage_initializes(self):
-        self.assertEqual(type(models.storage), FileStorage)
+        FileStorage.__objects[key] = obj
 
 
-class TestFileStorage_methods(unittest.TestCase):
-    """
-    Unittests for testing methods of the FileStorage class.
-    """
-
-    def setUp(self):
-        try:
-            os.rename("file.json", "tmp.json")
-        except FileNotFoundError:
-            pass
-
-    def tearDown(self):
-        try:
-            os.remove("file.json")
-        except FileNotFoundError:
-            pass
-        try:
-            os.rename("tmp.json", "file.json")
-        except FileNotFoundError:
-            pass
-        FileStorage._FileStorage__objects = {}
-
-    def test_all(self):
-        self.assertEqual(dict, type(models.storage.all()))
-
-    def test_all_with_arg(self):
-        with self.assertRaises(TypeError):
-            models.storage.all(None)
-
-    def test_new(self):
-        my_base_model = BaseModel()
-        my_user = User()
-        my_state = State()
-        my_place = Place()
-        my_city = City()
-        my_amenity = Amenity()
-        my_review = Review()
-        models.storage.new(my_base_model)
-        models.storage.new(my_user)
-        models.storage.new(my_state)
-        models.storage.new(my_place)
-        models.storage.new(my_city)
-        models.storage.new(my_amenity)
-        models.storage.new(my_review)
-        self.assertIn("BaseModel." + my_base_model.id, models.storage.all().keys())
-        self.assertIn(my_base_model, models.storage.all().values())
-        self.assertIn("User." + my_user.id, models.storage.all().keys())
-        self.assertIn(my_user, models.storage.all().values())
-        self.assertIn("State." + my_state.id, models.storage.all().keys())
-        self.assertIn(my_state, models.storage.all().values())
-        self.assertIn("Place." + my_place.id, models.storage.all().keys())
-        self.assertIn(my_place, models.storage.all().values())
-        self.assertIn("City." + my_city.id, models.storage.all().keys())
-        self.assertIn(my_city, models.storage.all().values())
-        self.assertIn("Amenity." + my_amenity.id, models.storage.all().keys())
-        self.assertIn(my_amenity, models.storage.all().values())
-        self.assertIn("Review." + my_review.id, models.storage.all().keys())
-        self.assertIn(my_review, models.storage.all().values())
-
-    def test_new_with_args(self):
-        with self.assertRaises(TypeError):
-            models.storage.new(BaseModel(), 1)
-
-    def test_new_with_None(self):
-        with self.assertRaises(AttributeError):
-            models.storage.new(None)
-
-    def test_save(self):
-        my_base_model = BaseModel()
-        my_user = User()
-        my_state = State()
-        my_place = Place()
-        my_city = City()
-        my_amenity = Amenity()
-        my_review = Review()
-        models.storage.new(my_base_model)
-        models.storage.new(my_user)
-        models.storage.new(my_state)
-        models.storage.new(my_place)
-        models.storage.new(my_city)
-        models.storage.new(my_amenity)
-        models.storage.new(my_review)
-        models.storage.save()
-        save_text = ""
-        with open("file.json", "r") as f:
-            save_text = f.read()
-            self.assertIn("BaseModel." + my_base_model.id, save_text)
-            self.assertIn("User." + my_user.id, save_text)
-            self.assertIn("State." + my_state.id, save_text)
-            self.assertIn("Place." + my_place.id, save_text)
-            self.assertIn("City." + my_city.id, save_text)
-            self.assertIn("Amenity." + my_amenity.id, save_text)
-            self.assertIn("Review." + my_review.id, save_text)
-
-    def test_save_with_arg(self):
-        with self.assertRaises(TypeError):
-            models.storage.save(None)
-
-    def test_reload(self):
-        my_base_model = BaseModel()
-        my_user = User()
-        my_state = State()
-        my_place = Place()
-        my_city = City()
-        my_amenity = Amenity()
-        my_review = Review()
-        models.storage.new(my_base_model)
-        models.storage.new(my_user)
-        models.storage.new(my_state)
-        models.storage.new(my_place)
-        models.storage.new(my_city)
-        models.storage.new(my_amenity)
-        models.storage.new(my_review)
-        models.storage.save()
-        models.storage.reload()
-        objs = FileStorage._FileStorage__objects
-        self.assertIn("BaseModel." + my_base_model.id, objs)
-        self.assertIn("User." + my_user.id, objs)
-        self.assertIn("State." + my_state.id, objs)
-        self.assertIn("Place." + my_place.id, objs)
-        self.assertIn("City." + my_city.id, objs)
-        self.assertIn("Amenity." + my_amenity.id, objs)
-        self.assertIn("Review." + my_review.id, objs)
-
-    def test_reload_with_arg(self):
-        with self.assertRaises(TypeError):
-            models.storage.reload(None)
+    def all(self):
+        """
+        Returns the __objects dictionary. 
+        It provides access to all the stored objects.
+        """
+        return  FileStorage.__objects
 
 
-if __name__ == "__main__":
-    unittest.main()
+    def save(self):
+        """
+        Serializes the __objects dictionary into 
+        JSON format and saves it to the file specified by __file_path.
+        """
+        all_objs = FileStorage.__objects
+
+        obj_dict = {}
+
+        for obj in all_objs.keys():
+            obj_dict[obj] = all_objs[obj].to_dict()
+
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
+            json.dump(obj_dict, file)
+
+    def reload(self):
+        """
+        This method deserializes the JSON file
+        """
+        if os.path.isfile(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
+                try:
+                    obj_dict = json.load(file)
+
+                    for key, value in obj_dict.items():
+                        class_name, obj_id = key.split('.')
+
+                        cls = eval(class_name)
+
+                        instance = cls(**value)
+
+                        FileStorage.__objects[key] = instance
+                except Exception:
+                    pass
